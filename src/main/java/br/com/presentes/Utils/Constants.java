c5e7e4b68f7588dce3b2c2fb234c0277b44bfb7a
Jhon Donavan <jhondonavan@gmail.com>
2020-06-20 14:54:10 -0300

    created utils and home class

---
+++ src/main/java/br/com/presentes/Utils/Constants.java
@@ -0,0 +1,7 @@
+package br.com.presentes.Utils;
+
+public class Constants {
+		
+	public static final String INDEX_VIEW = "index";
+
+}
--- src/main/java/br/com/presentes/controller/HomeController.java
+++ src/main/java/br/com/presentes/controller/HomeController.java
@@ -1,5 +1,12 @@
 package br.com.presentes.controller;
 
+import br.com.presentes.Utils.Constants;
+
 public class HomeController {
+	
+	public String Index() 
+	{
+		return Constants.INDEX_VIEW;
+	}
 
 }
