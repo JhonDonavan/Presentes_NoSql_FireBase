a434173f66f6abd178b6d65962b154b03ffdbd17
Jhon Donavan <jhondonavan@gmail.com>
2020-06-22 14:46:53 -0300

    swagger OK

--- src/main/java/br/com/presentes/SwaggerConfiguration.java
+++ src/main/java/br/com/presentes/SwaggerConfiguration.java
@@ -1,9 +1,15 @@
 package br.com.presentes;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,16 +19,23 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
 @Configuration
-//@EnableSwagger2
-public class SwaggerConfiguration {
+@EnableSwagger2
+public class SwaggerConfiguration  {
+
+	@Bean
+	public LinkDiscoverers discoverers() {
+		List<LinkDiscoverer> plugins = new ArrayList<>();
+		plugins.add(new CollectionJsonLinkDiscoverer());
+		return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+
+	}
 
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("br.com.presentes"))
-				.paths(PathSelectors.any())
-				.build()
+				.apis(RequestHandlerSelectors.basePackage("br.com.presentes")).paths(PathSelectors.any()).build()
 				.apiInfo(metaInfo());
 
 	}
@@ -35,4 +48,13 @@ public class SwaggerConfiguration {
 
 		return apiInfo;
 	}
+	
+	// for @EnableWebMvc
+	/* public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	*/
+
 }
